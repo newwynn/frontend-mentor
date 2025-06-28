60fd6ff004f28f7e7a08dcda505145363862efdc
newwynn <nevin.r@helixo.co>
2025-06-29 00:41:41 +0530

    feat: add interactive rating component with webpack production config

--- src/pages/interactive-rating-component/script.js
+++ src/pages/interactive-rating-component/script.js
@@ -1,4 +1,3 @@
-import '../../styles/interactive-rating-component/style.css';
 // Interactive Rating Component Script
 document.addEventListener('DOMContentLoaded', function () {
     const ratingCircles = document.querySelectorAll('.irc-rating-circle');
---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/images/[name][contenthash][ext]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+    }),
+  ],
+});
